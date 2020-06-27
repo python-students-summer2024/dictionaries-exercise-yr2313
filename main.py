dcb162c6ca385fba244fb66677df082f0f2098d9
bloombar <amos@cs.nyu.edu>
2020-06-27 12:24:57 -0400

    mac/windows-compatible file path

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ A virtual cookie shop.
 Do not modify this file.
 """
 
+from pathlib import Path
 import cookie_shop
 
 # get data from the CSV file into a list
-list_of_cookies = cookie_shop.bake_cookies('data/cookies.csv')
+filepath = Path("data/cookies.csv") # create a file path in a way that is both Mac- and Windows- compatible
+list_of_cookies = cookie_shop.bake_cookies(filepath)
 
 # open the cookie shop with the cookies read from the file
 # this must run the rest of the program as documented
